c3dd1dd70c2e6cbed778c9de44fa25db02f0299b
SamahSaeed400990 <121442474+SamahSaeed400990@users.noreply.github.com>
2025-02-17 12:57:50 +0200

    update

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
+import React ,{useEffect, useRef} from 'react'
 import  Helmet from '../Components/Helmet/Helmet'
 import CommonSection from '../Components/UI/common-section/CommonSection'
 import { Container , Row , Col } from 'reactstrap'
 import { Link } from 'react-router-dom'
  
 const Login = () => {
+  const loginEmailRef  = useRef()
+  const loginPasswordRef  = useRef()
+  useEffect(()=>{
+    loginEmailRef.current.focus()
+     
+  },[])
+  const handelSubmit = (e)=>{
+        e.preventDefault()
+  }
   return<>
     <Helmet title='Login'>
       <CommonSection title='Login'/>
@@ -12,17 +21,17 @@ const Login = () => {
         <Container>
           <Row>
             <Col lg='6' md='6' sm='12' className='m-auto text-center'>
-            <form  className='form mb-5'>
+            <form  className='form mb-5' onSubmit={handelSubmit}>
               <div className="form_group">
-                <input type="email" placeholder='Email' required />
+                <input type="email" placeholder='Email' required ref={loginEmailRef} />
               </div>
               <div className="form_group">
-                <input type="password" placeholder='Password' required />
+                <input type="password" placeholder='Password' required  ref={loginPasswordRef}/>
               </div>
               <button type='submit' className='addToCart_btn'>Login</button>
 
             </form>
-            <Link to='/register'>Already have an account? Create an acount</Link>
+            <Link to='/register'>Do not have an account? Create an acount</Link>
             </Col>
           </Row>
         </Container>
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
-
+import React ,{useEffect, useRef} from 'react'
+import  Helmet from '../Components/Helmet/Helmet'
+import CommonSection from '../Components/UI/common-section/CommonSection'
+import { Container , Row , Col } from 'reactstrap'
+import { Link } from 'react-router-dom'
+ 
 const Register = () => {
-  return (
-    <div>Register</div>
-  )
+  const registerNameRef  = useRef()
+  const registerEmailRef  = useRef()
+  const registerPasswordRef  = useRef()
+  useEffect(()=>{
+    registerNameRef.current.focus()
+  },[])
+  const handelSubmit = (e)=>{
+        e.preventDefault()
+  }
+  return<>
+    <Helmet title='Register'>
+      <CommonSection title='Register'/>
+      <section>
+        <Container>
+          <Row>
+            <Col lg='6' md='6' sm='12' className='m-auto text-center'>
+            <form  className='form mb-5' onSubmit={handelSubmit}>
+              <div className="form_group">
+                <input type="text" placeholder='Name' required ref={registerNameRef}/>
+              </div>
+              <div className="form_group">
+                <input type="email" placeholder='Email' required ref={registerEmailRef}/>
+              </div>
+              <div className="form_group">
+                <input type="password" placeholder='Password' required  ref={registerPasswordRef}/>
+              </div>
+              <button type='submit' className='addToCart_btn'>Register</button>
+
+            </form>
+            <Link to='/login'>Already have an account? Login</Link>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+    </Helmet>
+  </>
 }
 
 export default Register
